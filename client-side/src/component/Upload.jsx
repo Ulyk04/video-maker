14b3b52e18053ff860eaf1e3949e445070f83b1a
ulykpan <ulykpankajsar1@gmail.com>
2025-06-07 23:06:38 +0300

    commit

--- client-side/src/component/Upload.jsx
+++ client-side/src/component/Upload.jsx
@@ -1,8 +1,25 @@
+import { Card, CardContent, Input } from '@mui/material'
 import React from 'react'
 
 const Upload = () => {
+
+    const [videoTitle, setVideoTitle] = React.useState('');
+
   return (
-    <div>Upload</div>
+    <div>
+        <Card>
+            <CardContent>
+                <label style={{fontSize: '150%'}} >Upload Video file</label> <br /> <br />
+                <Input type='file' accept="video/*" sx={{width: '100%'}} ></Input>  <br /> <br />
+                <Input 
+                    placeholder="Video Title"
+                    sx={{width: '100%'}}
+                    value={videoTitle}
+                    onChange={(e) => setVideoTitle(e.target.value)}
+                />
+            </CardContent>
+        </Card>
+    </div>
   )
 }
 
