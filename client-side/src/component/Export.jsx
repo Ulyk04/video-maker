90d65785254446a52934f2199c7f474531b8379d
ulykpan <ulykpankajsar1@gmail.com>
2025-06-07 23:17:18 +0300

    commit

--- client-side/src/component/Export.jsx
+++ client-side/src/component/Export.jsx
@@ -5,21 +5,23 @@ import { motion } from 'framer-motion'
 import { CheckCircle } from '@mui/icons-material'
 
 const Export = () => {
+
+  const [videoTitle, setVideoTitle] = React.useState('')
   return (
     <div>
       <Card>
-            <CardContent className="p-4 space-y-4 text-center">
+            <CardContent style={{textAlign: 'center'}}>
               <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.5 }}
                 className="text-green-600 flex justify-center"
               >
-                <CheckCircle/>
+                <CheckCircle sx={{width: 100, height: 100 , mr: 2}} />
               </motion.div>
               <h2 className="text-xl font-semibold">Ready to Export!</h2>
               <p className="text-sm text-gray-500">Your video "{videoTitle || "Untitled"}" is ready.</p>
-              <Button variant="outline">Download Video</Button>
+              <Button variant="contained">Download Video</Button>
             </CardContent>
           </Card>
     </div>
